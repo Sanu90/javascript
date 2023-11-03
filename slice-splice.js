d13155e8bd30e882e29650ec5fec3f0a17ccc394
Sanup Divakaran <78775270+Sanu90@users.noreply.github.com>
2023-11-03 18:54:00 +0530

    Add files via upload

---
+++ slice-splice.js
@@ -0,0 +1,9 @@
+const array=['ab','cd','ef','gh','ij','kl']
+
+// const a1=array.slice(1,3);
+// console.log(a1);
+// console.log(array);
+
+const a2=array.splice(1,3,5,3,1);
+console.log(a2);
+console.log(array);
