a9741888183856ca2a10227b8dbf1f78474d73f4
Sanup Divakaran <sanukyr@gmail.com>
2024-05-10 14:26:26 +0530

    flat

--- flat.js
+++ flat.js
@@ -5,5 +5,11 @@ const array = [1, 2, 3, 4, [5, 6], [7, 8, 9], 10];
 let flatArray = array.flat();
 console.log(flatArray);
 
-// console.log("hey");
+// flat method does not flats the nested arrays inside.
 
+const array1 = [1, 2, 3, 4, [5, 6, [7, 8], 9], 10];
+
+let flatArray1 = array1.flat(Infinity);
+console.log(flatArray1);
+
+// If we use Infinity inside the flat method argument, it joins the nested arrays together to form a new array.
