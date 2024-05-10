335e2d0cca2faf68fe44bcfdcfe60895997b2ee3
Sanup Divakaran <sanukyr@gmail.com>
2024-05-10 18:02:21 +0530

    template literal

---
+++ template-literals.js
@@ -0,0 +1,7 @@
+let name = ["Hari", "Mahesh", "Jeevan", "Megha"];
+
+let job = ["painter", "carpenter", "driver", "teacher"];
+
+name.forEach((item1, item2) => {
+  console.log(`My name is ${item1}, and I work as a ${job[item2]}`);
+});
