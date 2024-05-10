7541f7b1d5af48b60f010e602ab595e53519ab67
Sanup Divakaran <sanukyr@gmail.com>
2024-05-10 19:31:09 +0530

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,21 @@
+// array //
+
+const array = ["car", "pen", "book", "mobile"];
+let [a, b, , c] = array;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//---- object ----//
+
+let object = {
+  company: "oppo",
+  employees: 5000,
+  HQ: "Mumbai",
+};
+let { company, employees, HQ } = object;
+
+console.log(company);
+console.log(employees);
+console.log(HQ);
