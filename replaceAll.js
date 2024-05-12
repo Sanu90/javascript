3bb7fb44c58bc096412a9af14c026b6c904e53f3
Sanup Divakaran <sanukyr@gmail.com>
2024-05-12 22:16:22 +0530

    replaceAll

---
+++ replaceAll.js
@@ -0,0 +1,4 @@
+const statement =
+  "This is a statement about javascript. The replaceAll function of javascript is used to replace all instances of the word.";
+
+console.log(statement.replaceAll("javascript", "***"));
