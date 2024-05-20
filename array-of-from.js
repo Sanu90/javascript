6b598f2ee3444fd274ad44c1373e7d8f36a0552d
Sanup Divakaran <sanukyr@gmail.com>
2024-05-20 09:56:28 +0530

    array-of-from

---
+++ array-of-from.js
@@ -0,0 +1,9 @@
+// Array.of() creates a new instance of array with variable number of arguments //
+
+const arr = Array.of(10, 5, 8, 3);
+console.log(arr);
+
+//Array.from() creates a new array instance from an array like or iterable instance. //
+
+const arr1 = Array.from("123");
+console.log(arr1);
