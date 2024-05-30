819a0b6b0d0a45c681bd5b68abf049da148b8f61
Sanup Divakaran <sanukyr@gmail.com>
2024-05-30 21:14:47 +0530

    multidimentional array

---
+++ multidimentionalArray.js
@@ -0,0 +1,16 @@
+// let one = [1, 2, 3];
+// let two = [4, 5, 6];
+// let three = [7, 8, 9];
+
+// let number = [one, two, three];
+// console.log(number);
+
+const array = [1, 2, 3];
+
+console.log(
+  array.map((value) => {
+    return [value * 1, value * 2];
+  })
+);
+
+//console.log(a)
