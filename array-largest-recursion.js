622bb8f8927677cdf47a26ed3a9eb24504d45bac
Sanup Divakaran <sanukyr@gmail.com>
2024-06-14 18:31:16 +0530

    array largest value Recursion

---
+++ array-largest-recursion.js
@@ -0,0 +1,13 @@
+let array = [10, 27, 25, 14, 19, 28]; // 10,14,19,25,27,28
+
+function maxMinRecursion(array, i, max, min) {
+  if (i == array.length) {
+    return [max, min];
+  }
+  max = Math.max(max, array[i]);
+  min = Math.min(min, array[i]);
+
+  return maxMinRecursion(array, i + 1, max, min);
+}
+
+console.log(maxMinRecursion(array, 0, 0, Infinity));
